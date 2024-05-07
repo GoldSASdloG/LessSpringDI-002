04bfef3cc0407ce05a715d7d386cb0aa8408cedb
GoldSASdloG <Procatulus@gmail.com>
2024-05-07 11:37:44 +0300

    добавили параметры видеокарты

--- src/main/java/org/example/CardVneshnya.java
+++ src/main/java/org/example/CardVneshnya.java
@@ -2,12 +2,38 @@ package org.example;
 
 public class CardVneshnya implements VideoCard{
 
+    private int series;
+    private String name;
+
+
     public CardVneshnya() {
         System.out.println("видяха создана");
+    }
+
+    public CardVneshnya(int series, String name) {
+        System.out.println("видяха создана");
+        this.series = series;
+        this.name = name;
     }
 
     @Override
     public void check() {
         System.out.println("Raysen Card Super 9080");
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main( String[] args ){
-        VideoCard card = new CardVneshnya();
+        VideoCard card = new CardVneshnya(6789, "electronika");
         Computer computer = new Computer(card);
 
         computer.getVideoCard().check();
